506572e6ad1a4b81b52a2ad355f010392f724630
abhishekkumarcoder <abhishekkumar9801777@gmail.com>
2024-07-04 21:01:07 +0530

    cmmited

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 //store a new product to the database
 Route::post('storeProduct',[ProductController::class,'storeProduct']);
